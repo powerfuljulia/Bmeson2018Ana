0aef4722512b4b00f625b3b75de9602ba8d1cb8f
Julia Manuela Silva <jusilva@lxplus779.cern.ch>
2019-07-14 22:30:56 +0200

    MCAsData9.sh

--- Bs/CrossSection/plotEff/plotEff/canvas1DhEff_PbPb.C
+++ Bs/CrossSection/plotEff/plotEff/canvas1DhEff_PbPb.C
@@ -1,7 +1,7 @@
 void canvas1DhEff_PbPb()
 {
 //=========Macro generated from canvas: canvas1D/
-//=========  (Fri Jul 12 00:48:36 2019) by ROOT version6.02/13
+//=========  (Sun Jul 14 03:07:38 2019) by ROOT version6.02/13
    TCanvas *canvas1D = new TCanvas("canvas1D", "",0,0,600,600);
    gStyle->SetOptStat(0);
    gStyle->SetOptTitle(0);
